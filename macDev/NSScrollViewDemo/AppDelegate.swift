e9288482d8fc54568e8c069f23ff24d690a9a026
ADDICE <addice.ch@gmail.com>
2017-11-16 16:39:09 +0800

    NSScrollVIewDemo Target

---
+++ macDev/NSScrollViewDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NSScrollViewDemo
+//
+//  Created by ADDICE on 2017/11/16.
+//  Copyright © 2017年 ADDICE. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ macDev/NSScrollViewDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NSScrollViewDemo
+//
+//  Created by ADDICE on 2017/11/16.
+//  Copyright © 2017年 ADDICE. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
