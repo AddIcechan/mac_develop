8131ea5071a7b6c753f5da1b036df791b0f6fb80
ADDICE <addice.ch@gmail.com>
2017-11-16 16:44:59 +0800

    完成 NSScrollViewDemo

--- macDev/NSScrollViewDemo/ViewController.swift
+++ macDev/NSScrollViewDemo/ViewController.swift
@@ -10,18 +10,45 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    lazy var bgScrollView: NSScrollView = {
+        let scrollView = NSScrollView()
+        
+        // scrollerStyle。overlay / legacy。 overlay 的效果，则是 scroller 背景透明，而 legacy 则是 独立出 scroller 的区域，看起来比较丑～～个人觉得😂
+        scrollView.scrollerStyle = .overlay
+        
+        // 滚动条的显示。用 IB 创建 scrollview 时，以下两个参数均为 true。但是 code 创建 scrollview 时，以下两个参数默认均为 false。很奇怪的设计。
+        scrollView.hasVerticalScroller = true
+        scrollView.hasHorizontalScroller = true
+        
+        // 滚动条的样式：light、 dark、default。default 的话，其实就是 dark
+        scrollView.scrollerKnobStyle = .dark
+        
+        //  bounce 的效果。elasticity 是弹性的含义。automatic\allowed\none。
+        scrollView.horizontalScrollElasticity = .automatic
+        scrollView.verticalScrollElasticity = .automatic
+        
+        return scrollView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(bgScrollView)
+        let contentView = NSView(frame: NSRect(x: 0, y: 0, width: 1000, height: 1000))
+        contentView.backgroundColor = .yellow
+        
+        bgScrollView.contentView.addSubview(contentView)
+        bgScrollView.documentView = contentView
+        
+        // 因为 坐标系起点 是在 左下角。因此想要让内容 起点 在左上角。可以选择进行 滚动 到底部，即 scrollview.documentView?.scroll(xxx)。为此，扩展一个方法。
+        bgScrollView.scrollToTop()
+        
     }
-
-    override var representedObject: Any? {
-        didSet {
-        // Update the view, if already loaded.
-        }
+    
+    override func viewWillLayout() {
+        super.viewWillLayout()
+        bgScrollView.frame = view.bounds
     }
-
 
 }
 
